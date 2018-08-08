fe8f0220d2c88c35865c63af9695d6dcf7a7f320
fmurtadho <fmurtadho@gmail.com>
2018-08-08 22:42:54 +0700

    angka palindrome

---
+++ exercise-6.js
@@ -0,0 +1,30 @@
+function angkaPalindrome(input){
+
+for(i=0;i<=input;i++){
+
+    input++;
+
+    var str = input.toString();
+
+    var dibalik=''
+
+    for(j=str.length-1;j>=0;j--){
+
+        dibalik = dibalik + str[j]
+
+        }
+
+    if(input==dibalik){
+
+        return input;
+
+        }
+    }
+}
+
+//TEST-CASE
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000)); // 1001
