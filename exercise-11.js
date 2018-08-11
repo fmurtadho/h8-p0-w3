fa740235f23250d9b7172c1186e02717d70605bf
fmurtadho <fmurtadho@gmail.com>
2018-08-11 12:28:31 +0700

    deret aritmatika

--- exercise-11.js
+++ exercise-11.js
@@ -25,3 +25,4 @@ console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
 console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
 console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
 console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
+//
